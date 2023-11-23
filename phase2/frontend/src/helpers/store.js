c7ac551703d05a9a2eb7abcaf33161df4883696c
Hon Chi Kit <kinoras@kinoras.com>
2023-11-24 03:07:47 +0800

    p2-front: feat: redux storage for userInfo

--- phase2/frontend/src/helpers/store.js
+++ phase2/frontend/src/helpers/store.js
@@ -1,7 +1,14 @@
 import { legacy_createStore as createStore } from 'redux'
 
+try {
+    JSON.parse(localStorage.getItem('auth') ?? '{}')
+} catch (err) {
+    localStorage.removeItem('auth')
+}
+
 const initialState = {
-    isDarkMode: localStorage.getItem('theme') === 'dark'
+    isDarkMode: localStorage.getItem('theme') === 'dark',
+    userInfo: JSON.parse(localStorage.getItem('auth') ?? '{}')
 }
 
 const reducer = (state = initialState, { type, value }) => {
@@ -11,6 +18,10 @@ const reducer = (state = initialState, { type, value }) => {
             document.body.style.backgroundColor = value ? '#000000' : '#F5F5F5'
             document.querySelector('meta[name="theme-color"]').setAttribute('content', value ? '#141414' : '#FFFFFF')
             return { ...state, isDarkMode: value }
+        case 'SET_USER':
+            if (value?.token) localStorage.setItem('auth', JSON.stringify(value))
+            else localStorage.removeItem('auth')
+            return { ...state, userInfo: value }
         default:
             return state
     }
