880f374990d2e2c5f16b2af7c4a4c3b8abb1a376
Hon Chi Kit <kinoras@kinoras.com>
2023-11-24 22:15:30 +0800

    p2-front: fix: connect handles error responds

--- phase2/frontend/src/helpers/context.js
+++ phase2/frontend/src/helpers/context.js
@@ -16,23 +16,19 @@ const ContextProvider = ({ children }) => {
     }
 
     const get = async (path, query) => {
-        try {
-            const queryString = new URLSearchParams(query).toString()
-            const response = await fetch(`${API_ROOT}${path}?${queryString}`, { method: 'GET', headers })
-            return response.json()
-        } catch (err) {
-            console.log(err)
-        }
+        const queryString = new URLSearchParams(query).toString()
+        const response = await fetch(`${API_ROOT}${path}?${queryString}`, { method: 'GET', headers })
+        const json = await response.json()
+        if (!response.ok) throw json
+        return json
     }
 
     const post = async (path, bodyObj) => {
-        try {
-            const body = JSON.stringify(bodyObj)
-            const response = await fetch(`${API_ROOT}${path}`, { method: 'POST', headers, body })
-            return response.json()
-        } catch (err) {
-            console.log(err)
-        }
+        const body = JSON.stringify(bodyObj)
+        const response = await fetch(`${API_ROOT}${path}`, { method: 'POST', headers, body })
+        const json = await response.json()
+        if (!response.ok) throw json
+        return json
     }
 
     return <GlobalContext.Provider value={{ connect: { get, post } }}>{children}</GlobalContext.Provider>
