3924fdb0f40c83fea59f81ae0bf965d56cc0e8fa
Hon Chi Kit <kinoras@kinoras.com>
2023-12-17 11:27:16 +0800

    p2-back: fix: error handling

--- phase2/backend/src/routes/index.js
+++ phase2/backend/src/routes/index.js
@@ -30,29 +30,33 @@ const routeList = [
 
 const router = async ({ method, path, token, query, body }) => {
     // Find route
-    const targetRoute = routeList.find((route) => {
-        return route.method === method && route.path === path
-    })
+    try {
+        const targetRoute = routeList.find((route) => {
+            return route.method === method && route.path === path
+        })
 
-    // Return error if route not found
-    if (!targetRoute || !targetRoute?.handler) {
-        return errorMessage(4041)
-    }
+        // Return error if route not found
+        if (!targetRoute || !targetRoute?.handler) {
+            return errorMessage(4041)
+        }
 
-    // Check token
-    const { valid, _id } = decodeToken(token)
-    if (targetRoute?.auth && !valid) {
-        return errorMessage(4011)
-    }
+        // Check token
+        const { valid, _id } = decodeToken(token)
+        if (targetRoute?.auth && !valid) {
+            return errorMessage(4011)
+        }
 
-    // Call handler for the route
-    switch (method) {
-        case 'GET':
-            return await targetRoute.handler(query, token, _id)
-        case 'POST':
-            return await targetRoute.handler(body, token, _id)
-        case 'DELETE':
-            return await targetRoute.handler(query, token, _id)
+        // Call handler for the route
+        switch (method) {
+            case 'GET':
+                return await targetRoute.handler(query, token, _id)
+            case 'POST':
+                return await targetRoute.handler(body, token, _id)
+            case 'DELETE':
+                return await targetRoute.handler(query, token, _id)
+        }
+    } catch (error) {
+        return errorMessage(5001, error)
     }
 }
 
