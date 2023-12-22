068008999118bc9ba20231f277bb221c04123db3
Hon Chi Kit <kinoras@kinoras.com>
2023-12-23 04:52:11 +0800

    p2-back: fix: server for development

--- phase2/backend/src/server.js
+++ phase2/backend/src/server.js
@@ -4,6 +4,7 @@ import { bodyLength, exit, formResponse, parseRequest } from './tools'
 import dotenv from 'dotenv'
 import fs from 'fs'
 import mongoose from 'mongoose'
+import net from 'net'
 import path from 'path'
 import tls from 'tls'
 
@@ -11,56 +12,73 @@ import tls from 'tls'
 dotenv.config()
 
 /* Environment check */
-if (!process.env.MONGO_URL) exit('Missing MONGO_URL!')
-
-const options = {
-    key: fs.readFileSync(path.join(__dirname, './private-key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, './public-cert.pem'))
+if (!process.env.MONGO_URL) {
+    exit('Missing MONGO_URL!')
 }
 
 // openssl genrsa -out private-key.pem 1024
 // openssl req -new -key private-key.pem -out csr.pem
 // openssl x509 -req -in csr.pem -signkey private-key.pem -out public-cert.pem
 
-/* Socket server */
-const server = tls.createServer(options, (socket) => {
-    let requestData = ''
-    let requestLength = 0
+const requestSolver = async (requestData, socket) => {
+    const { method, path, token, query, body } = parseRequest(requestData)
+    if (method === 'OPTIONS') {
+        // Preflight request
+        socket.write(formResponse.options())
+    } else if (path?.startsWith('/api/')) {
+        // API request
+        const { status, ...response } = (await router({ method, path, token, query, body })) ?? {}
+        socket.write(formResponse.api(status, response))
+    } else {
+        // Media request
+        const { status, type, content, ...response } = path?.startsWith('/media/')
+            ? await finder({ method, path })
+            : await staticHolder({ method, path })
+        if (status === 200) {
+            socket.write(formResponse.mediaHeader(status, type, content))
+            socket.write(content)
+        } else {
+            socket.write(formResponse.api(status, response))
+        }
+    }
+}
 
-    socket.on('data', async (data) => {
-        requestData += data.toString()
-        if (requestData.includes('\r\n\r\n')) {
-            const match = requestData.match(/Content-Length: (\d+)/)
-            requestLength = match ? parseInt(match[1], 10) : 0
-        }
-        if (bodyLength(requestData) >= requestLength) {
-            const { method, path, token, query, body } = parseRequest(requestData)
-            if (method === 'OPTIONS') {
-                // Preflight request
-                socket.write(formResponse.options())
-            } else if (path?.startsWith('/api/')) {
-                // API request
-                const { status, ...response } = (await router({ method, path, token, query, body })) ?? {}
-                socket.write(formResponse.api(status, response))
-            } else {
-                // Media request
-                const { status, type, content, ...response } = path?.startsWith('/media/')
-                    ? await finder({ method, path })
-                    : await staticHolder({ method, path })
-                if (status === 200) {
-                    socket.write(formResponse.mediaHeader(status, type, content))
-                    socket.write(content)
-                } else {
-                    socket.write(formResponse.api(status, response))
-                }
+if (process.env.MODE === 'DEPLOY' && (!process.env.SSL_KEY || !process.env.SSL_CERT)) {
+    exit('Missing SSL_KEY or SSL_CERT!')
+}
+
+/* Socket server */
+const server =
+    process.env.MODE === 'DEPLOY'
+        ? tls.createServer(
+            {
+                key: fs.readFileSync(path.join(__dirname, process.env.SSL_KEY)),
+                cert: fs.readFileSync(path.join(__dirname, process.env.SSL_CERT))
+            },
+            (socket) => {
+                let requestData = ''
+                let requestLength = 0
+                socket.on('data', async (data) => {
+                    requestData += data.toString()
+                    if (requestData.includes('\r\n\r\n')) {
+                        const match = requestData.match(/Content-Length: (\d+)/)
+                        requestLength = match ? parseInt(match[1], 10) : 0
+                    }
+                    if (bodyLength(requestData) >= requestLength) {
+                        await requestSolver(requestData, socket)
+                        requestData = ''
+                        requestLength = 0
+                    }
+                })
+                socket.on('error', (error) => console.log(error))
             }
-            // Reset
-            requestData = ''
-            requestLength = 0
-        }
-    })
-    socket.on('error', (error) => console.log(error))
-})
+        )
+        : net.createServer((socket) => {
+            socket.on('data', async (data) => {
+                await requestSolver(data.toString(), socket)
+            })
+            socket.on('error', (error) => console.log(error))
+        })
 
 /* database connection */
 mongoose.set('strictQuery', false)
